4de551ef652431634bb9d6e0a54966b29636bfc3
imocos <imi.cosi@gmail.com>
2025-02-23 19:53:26 +0100

    experimenting with the flatMap function for the next part

--- Exam Unit 2/Execution.js
+++ Exam Unit 2/Execution.js
@@ -54,3 +54,71 @@ function greet(name) {
 console.log(greet(name));
 
 //there might be a better way to do this
+
+//function to flatten an array of integers
+const array = [1, 2, 3,
+    4, 5, 6,
+    7, 8, 9
+];
+
+const arr = [
+    6410,
+    2831,
+    5049,
+    7554,
+    [
+      8707,
+      6940,
+      9517,
+      7565,
+      7522,
+      9242,
+      7972,
+      7064,
+      3441,
+      [
+        9960,
+        4966,
+        9368,
+        1634,
+        5150,
+        3709,
+        6660,
+        [
+          7155, 8056, 7834,
+          2639, 6601, 8063,
+          2390, 2544, 7022
+        ]
+      ],
+      2385,
+      573,
+      656,
+      733,
+      1620,
+      3626,
+      [
+        6274,
+        1935,
+        [ 6481, 928, 8291, 3196, 3431, 6058 ],
+        8010,
+        5052,
+        892,
+        3490,
+        2369,
+        951,
+        1606,
+        6763,
+        7260,
+        6122
+      ]
+    ],
+    5655,
+    4223
+  ];
+
+function flattenArray(arr) {
+    return arr.flatMap();
+
+}
+
+console.log(flattenArray(arr));
