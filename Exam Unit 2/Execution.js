588cf00395f7e0b840c6aa3a073d58024d8bfa52
imocos <imi.cosi@gmail.com>
2025-02-23 19:02:08 +0100

    made simple function to convert inch to mm and tested with console log

--- Exam Unit 2/Execution.js
+++ Exam Unit 2/Execution.js
@@ -9,3 +9,10 @@ function square(number) {
 console.log(square(2));
 console.log(square(16));
 
+//function that returns amount in mm assuming that it was given in inches (1 inch = 25.4 mm)
+
+function convertInchToMili(number) {
+    return number * 25.4;
+}
+
+console.log(convertInchToMili(10));
